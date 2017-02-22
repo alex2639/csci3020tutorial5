a0462dd8991e75d237227ecfb9cbbeba90306e68
alex2639 <mr.e_000@hotmail.com>
2017-02-21 20:57:44 -0500

    Add files via upload

--- question5.c
+++ question5.c
@@ -13,18 +13,20 @@ double total_grade=0;
 double total_bellcurve=0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_barrier_t barrier;
+
 student grades[NUM_THREADS];
 
 void *read_grades(){
   printf("Reading grades\n");
-  FILE *file=fopen("grades.txt","r");
-  char* line;
+  FILE *file;
+  file=fopen("grades.txt" , "r");
+  char line[256];
   int i=0;
-  while(scanf(file,line)!=EOF){
-    //grades[i].grade=stoi(line);
-    printf("%f\n", atof(line));
-    grades[i].grade=atof(line);
-    //printf("%f\n", atof(line));
+  while(fscanf(file,"%s",line)!=EOF){
+    //printf("%s\n", line);
+    grades[i].grade=atoi(line);
+    //printf("%f\n", grades[i].grade);
+    i++;
   }
   pthread_exit(NULL);
 }
@@ -33,11 +35,13 @@ void *save_bellcurve(void *arg){
   student *data=arg;
 
   pthread_mutex_lock(&mutex);
+  //printf("%f\n", data->grade);
+
   total_grade += data->grade;
   data->grade=data->grade*1.5;
   total_bellcurve += data->grade;
-  printf("%f\n", data->grade);
-  FILE *file=fopen("bellcurve.txt","wa");
+  //printf("%f\n", data->grade);
+  FILE *file=fopen("bellcurve.txt","a");
   fprintf(file, "%f\n", data->grade);
   pthread_mutex_unlock(&mutex);
 
@@ -46,27 +50,31 @@ void *save_bellcurve(void *arg){
 
 int main(void){
   pthread_t threads[NUM_THREADS];
-  //student grades[NUM_THREADS];
   pthread_t read_thread;
+  pthread_mutex_init(&mutex, NULL);
+  //pthread_barrier_init(&barrier,NULL,_XOPEN_SOURCE);
+
   int rc;
-  //printf("Reading grades\n");
-  rc=pthread_create(&read_thread,NULL,read_grades,NULL);
-  //pthread_join(read_thread,NULL);
-  pthread_barrier_wait(&barrier);
-  printf("Reading done\n");
+  rc=pthread_create(&read_thread,NULL,read_grades,(void *)0);
+  if(rc){
+    exit(-1);
+  }
+  pthread_join(read_thread,NULL);
+  //pthread_barrier_wait(&barrier);
+  //printf("Reading done\n");
 
   for(int i=0;i<NUM_THREADS;i++){
     student *arg=malloc(sizeof *arg);
     arg->grade=grades[i].grade;
     pthread_mutex_lock(&mutex);
     rc=pthread_create(&threads[i],NULL,save_bellcurve,arg);
-
     if(rc){
       exit(-1);
     }
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_unlock(&mutex);
     pthread_join(threads[i],NULL);
   }
+  printf("Total grade: %f\n", total_grade);
   double average_before=total_grade/NUM_THREADS;
   double average_after=total_bellcurve/NUM_THREADS;
   printf("Average before bellcurve: %f\n", average_before);
